d2ee844e9c3db7c0b5b7bb9f77b6e9894b4c854b
monnish-reddy <peta.monnish@gmail.com>
2025-09-04 11:37:51 -0400

    shell

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -7,7 +7,7 @@ then
     echo "please run the script with root access"
     exit 1 # give other thank 0 upto 127
 else
-    echo "runnig the script with root user
+    echo "runnig the script with root user"
 fi
 
 dnf install mysql -y
