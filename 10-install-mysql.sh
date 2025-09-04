f11191dd1db68f2da73488625ed4fdd2a667c161
monnish-reddy <peta.monnish@gmail.com>
2025-09-04 11:37:19 -0400

    shell

---
+++ 10-install-mysql.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then 
+    echo "please run the script with root access"
+    exit 1 # give other thank 0 upto 127
+else
+    echo "runnig the script with root user
+fi
+
+dnf install mysql -y
+
+
+
