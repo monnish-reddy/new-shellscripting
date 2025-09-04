6de5ccb0bc2487fa85e3c00615a893710f0233b7
monnish-reddy <peta.monnish@gmail.com>
2025-09-04 11:43:56 -0400

    shell

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -9,8 +9,23 @@ then
 else
     echo "runnig the script with root user"
 fi
+dnf list installed mysql
+if [ $? -ne 0]
+then 
+    echo "mysql is not installed going to insatlla"
+else
+    echo "mysql is already present "
+    exit 1
+fi
 
 dnf install mysql -y
+if [ $? -eq 0 ]
+then  
+    echo "installing mysql is success"
+else
+    echo "installing mysql is failure"
+    exit 1
+fi
 
 
 
